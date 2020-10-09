1466c1d0216ff73593469d3765aebd2b362c7790
rinelson456 <nel15031@byui.edu>
2020-10-09 16:46:35 -0600

    week04

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents: Document[] = [new Document(1, 'Document Test', 'This is a test', 'someurl.com', 'child'),
+                          new Document(2, 'A Document Test', 'This is another test', 'someurl.com', 'child')];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectedDocumentEvent(document: Document){
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
--- cms/src/app/header/header.component.ts
+++ cms/src/app/header/header.component.ts
@@ -1,13 +1,16 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html'
-})
-export class HeaderComponent {
-  @Output() featureSelected = new EventEmitter<string>();
+    selector: 'app-header',
+    templateUrl: './header.component.html'
 
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
-}
+})
+ export class HeaderComponent{
+    @Output() featureSelected = new EventEmitter<string>();
+
+     onSelect(feature: string){
+         this.featureSelected.emit(feature);
+
+     }
+
+ }
