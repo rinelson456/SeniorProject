521b29661dadfeb88dd190797a461beb7d3c99fc
rinelson456 <nel15031@byui.edu>
2021-03-27 12:01:29 -0600

    logo

--- recipeBook/src/app/recipes/recipe.service.ts
+++ recipeBook/src/app/recipes/recipe.service.ts
@@ -24,7 +24,7 @@ export class RecipeService {
 
     
     getRecipes(currentPage: number){
-        let length;
+        let ingredientArray: any[] = [];
         this.ingredient = this.slService.getIngredients();
         if(this.ingredient.length == 0){
              this.http
@@ -33,55 +33,47 @@ export class RecipeService {
                 (ingredients: any[] ) => {
                   let ingredientString = JSON.stringify(ingredients);
                   let parse = JSON.parse(ingredientString);
-                  length = parse.length
+                  let length = parse.length
                   for(let i = 0; i < length; i++){
                     this.ingredient[i] = ({
                         name: parse[i].name,
                         amount: parse[i].amount
-                    }); 
-
-                    this.slService.ingredientsChanged.next(this.ingredient.slice()); 
-                  }     
+    
+                    });
+                } 
+                    for(let j= 0; j < length; j++){
+                            ingredientArray[j] = this.ingredient[j].name
+                        }
+                        const queryParams = `?i=${ingredientArray}&page=${currentPage}`;
+                        return this.http
+                        .get('http://localhost:3000/recipes/' + queryParams).subscribe(
+                            // success method
+                            (recipes: any[] ) => {
+                                console.log('http://localhost:3000/recipes/' + queryParams);
+                              let recipeString = JSON.stringify(recipes);
+                              let parse = JSON.parse(recipeString);
+                              for(let i = 0; i < 10; i++){
+                                this.recipes[i] = ({
+                                  name: parse.results[i].title,
+                                  description: parse.results[i].href,
+                                  imagePath: parse.results[i].thumbnail,
+                                  ingredients: parse.results[i].ingredients
+                                });
+                                console.log('here'); 
+                                this.recipesChanged.next(this.recipes.slice()); 
+                                this.slService.ingredientsChanged.next(this.ingredient.slice());
+                              }     
+                          },
+                            // error method
+                            (error: any) => {
+                               console.log(error);
+                            });    
               },
                 // error method
                 (error: any) => {
                    console.log(error);
                 });
         }
-
-        // let ingredientArray: any[] = [];
-
-        // console.log(this.ingredient) 
-
-        // for(let j= 0; j < 10; j++){
-        //     console.log('here1')
-        //     console.log(this.ingredient[j])
-        //     ingredientArray[j] = this.ingredient[j].name
-        //     console.log(ingredientArray[j])
-        // }
-        console.log(this.ingredient)
-        const queryParams = `?i=${this.ingredient}&page=${currentPage}`;
-        return this.http
-        .get('http://localhost:3000/recipes/' + queryParams).subscribe(
-            // success method
-            (recipes: any[] ) => {
-                console.log('http://localhost:3000/recipes/' + queryParams);
-              let recipeString = JSON.stringify(recipes);
-              let parse = JSON.parse(recipeString);
-              for(let i = 0; i < 10; i++){
-                this.recipes[i] = ({
-                  name: parse.results[i].title,
-                  description: parse.results[i].href,
-                  imagePath: parse.results[i].thumbnail,
-                  ingredients: parse.results[i].ingredients
-                }); 
-                this.recipesChanged.next(this.recipes.slice()); 
-              }     
-          },
-            // error method
-            (error: any) => {
-               console.log(error);
-            }); 
     }
 
 
